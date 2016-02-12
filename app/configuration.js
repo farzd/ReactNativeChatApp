0e5e39977f07b336385acec647d36a5cd8404b63
Farzad Qasim <qasimf@tcd.ie>
2016-02-12 17:27:07 +0000

    Restart

--- app/configuration.js
+++
@@ -1,4 +0,0 @@
-export default {
-    facebookReadPermissions: ['public_profile', 'email', 'user_friends'],
-    facebookUserRequest: '/me/?fields=name,picture.width(300)'
-};
--- index.ios.js
+++
@@ -1,4 +0,0 @@
-import React from 'react-native';
-import App from './app/containers/app';
-const { AppRegistry } = React;
-AppRegistry.registerComponent('ReactNativeChatApp', () => App);
