a8442697530d7f640950374c1aca092aa5a9c7a2
Farzad Qasim <qasimf@tcd.ie>
2016-02-15 17:45:25 +0000

    Updating server to broadcast

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 3000 });
+  , wss = new WebSocketServer({host:'localhost', port: 3000 });
+
 
 const users = [];
 const colors = ['#F974A0', '#F50057', '#2196F3', '#009688', '#EF6C00', '#01579B', '#F44336' ];
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
@@ -16,9 +23,11 @@ wss.on('connection', function connection(ws) {
     }
 
     const currentUser = users.indexOf(users[0]);
-    ws.send(data + dilem + colors[currentUser]);
+    wss.broadcast(data + dilem + colors[currentUser]);
     console.log('sending:', data + dilem + colors[currentUser]);
   });
+
+
 
   ws.send('server{<>}Welcome {<>}{<>}#5D70C2');
 });
